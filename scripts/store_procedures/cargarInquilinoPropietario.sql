1cdb8bfe5d6f7b2b139af37b0ce1def41ac467bf
agostinasvlas <111026978+agostinasvlas@users.noreply.github.com>
2025-10-28 20:27:59 -0300

    Update cargarInquilinoPropietario.sql

--- scripts/store_procedures/cargarInquilinoPropietario.sql
+++ scripts/store_procedures/cargarInquilinoPropietario.sql
@@ -3,13 +3,24 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER PROCEDURE [dbo].[cargarInquilinoPropietario]
+CREATE PROCEDURE [dbo].[cargarInquilinoPropietario]
     @RutaArchivoCSV NVARCHAR(255)
 AS
 BEGIN
     SET NOCOUNT ON;
    
     DECLARE @BulkInsertSQL NVARCHAR(MAX);
+    DECLARE @ExisteArchivo INT;
+
+    EXEC master.dbo.xp_fileexist @RutaArchivoCSV, @ExisteArchivo OUTPUT;
+
+    IF @ExisteArchivo = 0
+    BEGIN
+        PRINT 'ERROR: El archivo no existe en la ruta especificada: ' + @RutaArchivoCSV;
+        RETURN;
+    END
+
+    PRINT 'Archivo encontrado: ' + @RutaArchivoCSV;
 
     DROP TABLE IF EXISTS #persona_staging;
 
@@ -54,9 +65,9 @@ BEGIN
     )
     SELECT
         dni_csv,
-        TRIM(nombre_csv),
-        TRIM(apellido_csv),
-        TRIM(email_csv),
+        dbo.corregirTexto(nombre_csv),
+        dbo.corregirTexto(apellido_csv),
+        dbo.corregirTexto(email_csv),
         TRIM(telefono_csv),
         TRIM(cvu_cbu_csv) AS cuenta, 
         CASE
@@ -70,3 +81,35 @@ BEGIN
     DECLARE @FilasInsertadas INT = @@ROWCOUNT;
     PRINT 'Proceso de importación completado. Filas insertadas en dbo.persona: ' + CAST(@FilasInsertadas AS VARCHAR(10));
 END
+create function corregirTexto (@texto varchar(50))
+returns varchar(50)
+as
+begin
+	declare @resultado varchar(50);
+
+	SET @resultado = LTRIM(RTRIM(@texto)); --saca espacios al inicio y al final
+    SET @resultado = REPLACE(@resultado, 'ñ', 'n'); -- cambia ñ por n
+	---saca las tildes
+	SET @resultado = REPLACE(@resultado, 'á', 'a');
+    SET @resultado = REPLACE(@resultado, 'é', 'e');
+    SET @resultado = REPLACE(@resultado, 'í', 'i');
+    SET @resultado = REPLACE(@resultado, 'ó', 'o');
+    SET @resultado = REPLACE(@resultado, 'ú', 'u');
+    SET @resultado = REPLACE(@resultado, 'Á', 'A');
+    SET @resultado = REPLACE(@resultado, 'É', 'E');
+    SET @resultado = REPLACE(@resultado, 'Í', 'I');
+    SET @resultado = REPLACE(@resultado, 'Ó', 'O');
+    SET @resultado = REPLACE(@resultado, 'Ú', 'U');
+    
+
+	if  charindex('@',@resultado) > 0 --si es email elimina espacios intermedios, y lo pone en minuscula
+	begin
+		SET @resultado = REPLACE(@resultado, ' ', '');
+		SET @resultado = LOWER(@resultado);
+	end
+	else -- si es texto normal lo pone todo mayuscula
+		SET @resultado = UPPER(@resultado);
+
+
+	return @resultado
+end
