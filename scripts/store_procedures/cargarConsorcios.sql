aaac0169f06f6d2bcebe6f83702c619af72a7d35
Agostina <agostina.salas.as@gmail.com>
2025-10-31 10:11:39 -0300

    Añadi script para cargar consorcios

---
+++ scripts/store_procedures/cargarConsorcios.sql
@@ -0,0 +1,71 @@
+CREATE PROCEDURE [dbo].[sp_importarConsorcios]
+    @RutaArchivoCSV NVARCHAR(255)
+AS
+BEGIN
+    SET NOCOUNT ON;
+   
+    DECLARE @BulkInsertSQL NVARCHAR(MAX);
+	DECLARE @ExisteArchivo INT;
+
+    EXEC master.dbo.xp_fileexist @RutaArchivoCSV, @ExisteArchivo OUTPUT;
+
+    IF @ExisteArchivo = 0
+    BEGIN
+        PRINT 'ERROR: El archivo no existe en la ruta especificada: ' + @RutaArchivoCSV;
+        RETURN;
+    END
+
+    PRINT 'Archivo encontrado: ' + @RutaArchivoCSV;
+
+    DROP TABLE IF EXISTS #consorcios_staging;
+
+    CREATE TABLE #consorcios_staging (
+        id_consorcio_csv CHAR(11),
+        nombre_csv varchar(20),
+        direccion_csv varchar(20),
+        unidades_csv char(4),
+		m2_csv varchar(10),
+    );
+
+    SET @BulkInsertSQL = 
+        N'BULK INSERT #consorcios_staging ' +
+        N'FROM ''' + @RutaArchivoCSV + N''' ' + 
+        N'WITH ( ' +
+        N'    FIELDTERMINATOR = '','', ' +
+        N'    ROWTERMINATOR = ''\n'', ' +
+        N'    FIRSTROW = 2, ' +
+        N'    CODEPAGE = ''ACP'' ' +
+        N');';
+
+    BEGIN TRY
+        EXECUTE sp_executesql @BulkInsertSQL;
+
+    END TRY
+    BEGIN CATCH
+        DECLARE @ErrorMessage NVARCHAR(MAX) = ERROR_MESSAGE();
+        PRINT 'ERROR en BULK INSERT: ' + @ErrorMessage;
+        RETURN;
+    END CATCH
+
+    INSERT INTO dbo.consorcios (
+        id_consorcio,
+        nombre,
+        direccion,
+        unidades,
+		m2
+    )
+    SELECT
+        RIGHT(id_consorcio_csv, len(id_consorcio_csv)-charindex(' ',id_consorcio_csv)),
+        nombre_csv,
+        direccion_csv,
+        try_cast(unidades_csv as int),
+		try_cast(m2_csv as decimal)
+    FROM
+        #consorcios_staging
+    WHERE id_consorcio_csv IS NOT NULL;
+
+    DECLARE @FilasInsertadas INT = @@ROWCOUNT;
+    PRINT 'Proceso de importación completado. Filas insertadas en dbo.consorcios: ' + CAST(@FilasInsertadas AS VARCHAR(10));
+
+	DROP TABLE IF EXISTS #consorcios_staging
+END
