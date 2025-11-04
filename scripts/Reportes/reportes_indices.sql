dab61797ff53d4e8db75b1d7b43dc061b3297ee5
keydasilva7 <keialvarezdasilva@hotmail.com>
2025-11-04 18:44:34 -0300

    Update reportes_indices.sql
    Update de Indices por cambio en las tablas

--- scripts/Reportes/reportes_indices.sql
+++ scripts/Reportes/reportes_indices.sql
@@ -1,38 +1,34 @@
 --INDICES PARA OPTIMIZAR REPORTES
 
---Reporte 1 y 2: Búsqueda y agrupamiento por fecha de pago y consorcio
-CREATE INDEX IX_Pago_FechaPago ON dbo.Pago(FechaPago);
+--Reporte 1 y 2: BÃºsqueda y agrupamiento por fecha de pago y consorcio
+CREATE INDEX IX_Pago_FechaPago ON tpo.Pago(FechaPago);
 GO
-CREATE INDEX IX_Expensa_IdConsorcio ON dbo.Expensa(IdConsorcio);
-GO
-
---Reporte 2 y 3: Filtrado por tipo de gasto
-CREATE INDEX IX_DetalleExpensa_TipoGasto ON dbo.DetalleExpensa(TipoGasto);
+CREATE INDEX IX_Expensa_IdConsorcio ON tpo.Expensa(IdConsorcio);
 GO
 
---Reporte 4: Agrupación por mes en gastos ordinarios
-CREATE INDEX IX_GastoOrdinario_Mes ON dbo.GastoOrdinario(Mes);
+--Reporte 2 y 3: uso  de IdDetalleExp e IdExpensa en joins
+CREATE INDEX IX_DetalleExpensa_IdExpensa ON tpo.DetalleExpensa(IdExpensa);
+GO
+CREATE INDEX IX_Pago_IdDetalleExp ON tpo.Pago(IdDetalleExp);
 GO
 
---Reporte 5: Relación Persona - UnidadFuncional - Pagos
-CREATE INDEX IX_Persona_Cuenta ON dbo.Persona(Cuenta);
+--Reporte 4: AgrupaciÃ³n por mes en gastos ordinarios
+CREATE INDEX IX_GastoOrdinario_Mes ON tpo.GastoOrdinario(Mes);
 GO
-CREATE INDEX IX_Pago_IdUf ON dbo.Pago(IdUf);
-GO
-CREATE INDEX IX_DetalleExpensa_IdExpensa ON dbo.DetalleExpensa(IdExpensa);
+CREATE INDEX IX_GastoOrdinario_IdConsorcio ON tpo.GastoOrdinario(IdConsorcio);
 GO
 
---Reporte 6: Días entre pagos (IdUf + FechaPago)
-CREATE INDEX IX_Pago_IdUf_FechaPago ON dbo.Pago(IdUf, FechaPago);
+--Reporte 5: RelaciÃ³n Persona - UnidadFuncional - Pagos
+CREATE INDEX IX_Persona_DNI ON tpo.Persona(DNI);
+GO
+CREATE INDEX IX_UnidadFuncional_IdPropietario ON tpo.UnidadFuncional(IdPropietario);
+GO
+CREATE INDEX IX_UnidadFuncional_Cuenta ON tpo.UnidadFuncional(Cuenta);
+GO
+CREATE INDEX IX_Pago_IdUf ON tpo.Pago(IdUf);
 GO
 
---Reporte 1: va a los pagos y expensas relevantes
---Reporte 2: indice compuesto
---Reportes 2 y 3: encuentra rapidamente solo los ordinarios o extraordinarios
---Reporte 4: agrupa por mes, al estar indexado por mes agrupa mas rapido
---Reporte 5: como hay multiples joins entre estas tablas: aceleran las busquedas por cada join
---Busca persona x cuenta
---Busca pagos x uf
---Busca detalles x expensa
---Reporte 6:indice compuesto, el reporte filtra por iduf y ordena por fechapago
---esto hace que sea nas rapido en un solo indice
+--Reporte 6: DÃ­as entre pagos (IdUf + FechaPago)
+CREATE INDEX IX_Pago_IdUf_FechaPago ON tpo.Pago(IdUf, FechaPago);
+GO
+
