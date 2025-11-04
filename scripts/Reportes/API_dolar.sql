d49a49083675403e03b86b11427b2109ed5d31e0
keydasilva7 <keialvarezdasilva@hotmail.com>
2025-11-04 00:53:36 -0300

    API

---
+++ scripts/Reportes/API_dolar.sql
@@ -0,0 +1,56 @@
+--Habilitamos acceso a datos externos
+
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+EXEC sp_configure 'Ole Automation Procedures', 1;
+RECONFIGURE;
+
+use expensas
+go
+
+
+
+CREATE TABLE cotizacion_dolar (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    fecha DATETIME DEFAULT GETDATE(),
+    tipo VARCHAR(10),        -- 'oficial' o 'blue'
+    compra DECIMAL(10,2),
+    venta DECIMAL(10,2),
+    promedio DECIMAL(10,2)
+);
+
+CREATE OR ALTER PROCEDURE sp_actualizar_cotizacion_dolar
+AS
+BEGIN
+    DECLARE @Object INT,
+            @ResponseText NVARCHAR(MAX),
+            @Json NVARCHAR(MAX);
+
+    -- Crear el objeto HTTP
+    EXEC sp_OACreate 'MSXML2.XMLHTTP', @Object OUT;
+    EXEC sp_OAMethod @Object, 'open', NULL, 'GET', 
+                     'https://api.bluelytics.com.ar/v2/latest', false;
+    EXEC sp_OAMethod @Object, 'send';
+    EXEC sp_OAGetProperty @Object, 'responseText', @ResponseText OUT;
+    EXEC sp_OADestroy @Object;
+
+    SET @Json = @ResponseText;
+
+    -- Insertar los valores desde el JSON usando OPENJSON
+    INSERT INTO cotizacion_dolar (tipo, compra, venta, promedio)
+    SELECT 
+        'oficial',
+        JSON_VALUE(@Json, '$.oficial.value_buy'),
+        JSON_VALUE(@Json, '$.oficial.value_sell'),
+        JSON_VALUE(@Json, '$.oficial.value_avg')
+    UNION ALL
+    SELECT 
+        'blue',
+        JSON_VALUE(@Json, '$.blue.value_buy'),
+        JSON_VALUE(@Json, '$.blue.value_sell'),
+        JSON_VALUE(@Json, '$.blue.value_avg');
+END;
+GO
+
+EXEC sp_actualizar_cotizacion_dolar;
+SELECT * FROM cotizacion_dolar;
