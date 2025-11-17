841137c8813333b67cea12583a0875d60b283a6c
keydasilva7 <keialvarezdasilva@hotmail.com>
2025-11-17 00:41:55 -0300

    Update reportes_ejecucion.sql

--- scripts/Reportes/reportes_ejecucion.sql
+++ scripts/Reportes/reportes_ejecucion.sql
@@ -1,18 +1,28 @@
 --INVOCACIÓN A LOS SP DE REPORTES
 
 -- Reporte 1
-EXEC tpo.sp_reporte_flujo_caja '2025-01-01', '2025-12-31', 1;
+EXEC tpo.sp_reporte_flujo_caja 
+    @FechaInicio = '2025-01-01', 
+    @FechaFin = '2025-12-31', 
+    @IdConsorcio = 1;
 GO
 
 -- Reporte 2
-EXEC tpo.sp_reporte_recaudacion_mensual 2025, 1;
+EXEC tpo.sp_reporte_recaudacion_mensual 
+    @Anio = 2025, 
+    @IdConsorcio = 1;
 GO
 
 -- Reporte 3
-EXEC tpo.sp_reporte_recaudacion_desA '2025-01-01', '2025-12-31', 1;
+EXEC tpo.sp_reporte_recaudacion_desA 
+    @FechaInicio = '2025-01-01', 
+    @FechaFin = '2025-12-31', 
+    @IdConsorcio = 1;
+GO
 
 -- Reporte 4
-EXEC tpo.sp_reporte_top5_gastos_ingresos 1;
+EXEC tpo.sp_reporte_top5_gastos_ingresos 
+    @IdConsorcio = 1;
 GO
 
 -- Reporte 5
@@ -20,6 +30,10 @@ EXEC tpo.sp_reporte_top3_morosidad;
 GO
 
 -- Reporte 6
-EXEC tpo.sp_reporte_dias_entre_pagos 1, 1, '2025-01-01';
+EXEC tpo.sp_reporte_dias_entre_pagos 
+    @IdConsorcio = 1,
+    @IdUf = 1,
+    @FechaDesde = '2025-01-01';
 GO
+
 
