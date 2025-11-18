452d1b63cfb9025d9601d012c852a8f101017696
agostinasvlas <111026978+agostinasvlas@users.noreply.github.com>
2025-11-18 20:43:59 -0300

    Update 20_permisos.sql

--- scripts/roles_permisos/20_permisos.sql
+++ scripts/roles_permisos/20_permisos.sql
@@ -1,3 +1,14 @@
+/*
+Este script crea roles y asigna permisos
+18/11/2025
+Com2900G10
+Grupo 10
+Bases de datos aplicadas
+Integrantes:
+-Kevin Maciel
+-Marcos kouvach
+-Agostina salas
+-Keila Álvarez Da Silva*/
 USE Com2900G10;
 GO
 
